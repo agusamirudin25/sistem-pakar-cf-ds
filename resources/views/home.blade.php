ee646f28097122b19a286ba5dee82168aab5e617
Agus Amirudin <agusamirudin1996@gmail.com>
2023-06-23 21:10:27 +0700

    init project

---
+++ resources/views/home.blade.php
@@ -0,0 +1,60 @@
+@extends('master')
+
+@section('home')
+    <main id="main" class="main">
+
+        <div class="pagetitle">
+            <h1>Dashboard</h1>
+        </div><!-- End Page Title -->
+
+        <section class="section dashboard">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="row">
+
+                        <!-- Sales Card -->
+                        <div class="col-xxl-4 col-md-4">
+                            <div class="card info-card sales-card">
+                                <div class="card-body">
+                                    <h5 class="card-title">Total Jenis</h5>
+                                    <div class="d-flex align-items-center">
+                                        <div
+                                            class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                                            <i class="bi bi-cart"></i>
+                                        </div>
+                                        <div class="ps-3">
+                                            <h6>{{ $total_jenis }}</h6>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div><!-- End Sales Card -->
+
+                        <!-- Revenue Card -->
+                        <div class="col-xxl-4 col-md-4">
+                            <div class="card info-card revenue-card">
+                                <div class="card-body">
+                                    <h5 class="card-title">Total Indikasi</h5>
+
+                                    <div class="d-flex align-items-center">
+                                        <div
+                                            class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                                            <i class="bi bi-currency-dollar"></i>
+                                        </div>
+                                        <div class="ps-3">
+                                            <h6>{{ $total_identi }}</h6>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div><!-- End Revenue Card -->
+                    </div>
+                </div><!-- End Left side columns -->
+
+            </div>
+        </section>
+
+    </main><!-- End #main -->
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,71 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DiagnosisController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\MasterIdentiController;
+use App\Http\Controllers\MasterJenisController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\RuleController;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('login', [AuthController::class, 'index'])->name('login');
+Route::post('proses_login', [AuthController::class, 'proses_login'])->name('proses_login');
+Route::get('logout', [AuthController::class, 'logout'])->name('logout');
+
+//Register
+Route::get('register', [RegisterController::class, 'register'])->name('register');
+Route::post('register/action', [RegisterController::class, 'actionregister'])->name('actionregister');
+
+//Role Pakar
+Route::group(['middleware' => ['auth']], function () {
+    Route::get('/', [HomeController::class, 'index'])->name('home');
+
+    //Role admin
+    Route::group(['middleware' => ['cek_login:admin|pakar']], function () {
+        Route::resource('users', UserController::class);
+
+        //Master Jenis Serangan
+        Route::get('master_jenis', [MasterJenisController::class, 'index'])->name('master_jenis.index')->withoutMiddleware('cek_login:admin');
+        Route::post('master_jenis', [MasterJenisController::class, 'store'])->name('master_jenis.store');
+        Route::get('master_jenis/form', [MasterJenisController::class, 'create'])->name('master_jenis.create');
+        Route::get('master_jenis/{id}', [MasterJenisController::class, 'edit'])->name('master_jenis.edit');
+        Route::put('master_jenis/{id}', [MasterJenisController::class, 'update'])->name('master_jenis.update');
+        Route::delete('master_jenis/{id}', [MasterJenisController::class, 'delete'])->name('master_jenis.delete');
+
+        //Master Identifikasi
+        Route::get('master_identi', [MasterIdentiController::class, 'index'])->name('master_identi.index');
+        Route::post('master_identi', [MasterIdentiController::class, 'store'])->name('master_identi.store');
+        Route::get('master_identi/form', [MasterIdentiController::class, 'create'])->name('master_identi.create');
+        Route::get('master_identi/{id}', [MasterIdentiController::class, 'edit'])->name('master_identi.edit');
+        Route::put('master_identi/{id}', [MasterIdentiController::class, 'update'])->name('master_identi.update');
+        Route::delete('master_identi/{id}', [MasterIdentiController::class, 'delete'])->name('master_identi.delete');
+
+        //Transaksi Rule
+        Route::get('t_rule', [RuleController::class, 'index'])->name('t_rule.index');
+        Route::post('t_rule', [RuleController::class, 'store'])->name('t_rule.store');
+        Route::get('t_rule/form', [RuleController::class, 'create'])->name('t_rule.create');
+        Route::get('t_rule/{id}', [RuleController::class, 'edit'])->name('t_rule.edit');
+        Route::put('t_rule/{id}', [RuleController::class, 'update'])->name('t_rule.update');
+        Route::delete('t_rule/{id}', [RuleController::class, 'delete'])->name('t_rule.delete');
+
+        //Transaksi Diagnosis
+        Route::get('hasil-diagnosis', [DiagnosisController::class, 'hasilDiagnosis'])->name('diagnosis.hasil');
+    });
+    Route::get('diagnosis', [DiagnosisController::class, 'index'])->name('diagnosis.index');
+    Route::get('outputDiagnosis', [DiagnosisController::class, 'output'])->name('diagnosis.output');
+    Route::post('prosesDiagnosis', [DiagnosisController::class, 'prosesDiagnosis'])->name('diagnosis.prosesDiagnosis');
+});
